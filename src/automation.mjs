83ebbb4d5990a7ce37367b1917cd54776249f749
Lucas deGomez <lucasdegomez@gmail.com>
2019-05-25 22:03:29 +0200

    Url generator apply

--- src/automation.mjs
+++ src/automation.mjs
@@ -1,11 +1,20 @@
 import puppeteer from 'puppeteer';
+import urlGenerator from './adapters/urlGenerator';
 
-export default async function webLaunch(query) {
+export default async function webLaunch(artist) {
   const browser = await puppeteer.launch({
     headless: false
   });
+  const url = urlGenerator(artist);
+  console.log(url)
   const page = await browser.newPage();
-  await page.goto(`https://genius.com/search?q=${query}`);
+  await page.goto(url);
+  const linkHandlers = await page.$x("//a[contains(., 'more songs')]");
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error("Link not found");
+  }
 
-  await browser.close();
+  // await browser.close();
 };
