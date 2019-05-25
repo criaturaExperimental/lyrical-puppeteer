8dec9e29e2247ed0e0fe0aa98c88a33c716ddb6d
Lucas deGomez <lucasdegomez@gmail.com>
2019-05-25 22:03:48 +0200

    Query adapter

--- src/app.mjs
+++ src/app.mjs
@@ -1,14 +1,12 @@
 import AskForInput from './readline';
 import automation from './automation';
-import queryAdapter from './adapters/queryAdapter';
 
 const question = new AskForInput();
 
 const query = question.ask('Name of artist or band lyrics you are looking for: ');
 
 query.then((answer) => {
-  answer = queryAdapter(answer);
-  console.log(answer);
+  console.log(`Looking for ${answer} lyrics`);
   automation(answer);
 })
 
